2617ccd0ab8d52b6a60dc2174421d7b9707cf103
axeljk <axeljkern@yahoo.com>
2022-08-19 23:24:55 -0400

    Fixes naming error in model parameters.

--- models/Thought.js
+++ models/Thought.js
@@ -25,6 +25,6 @@ const thoughtSchema = new Schema({
 
 thoughtSchema.virtual("reactionCount").get(() => this.reactions.length);
 
-const Thought = model("thought", thoughtSchema);
+const Thought = model("Thought", thoughtSchema);
 
 module.exports = Thought;
--- models/User.js
+++ models/User.js
@@ -36,6 +36,6 @@ const userSchema = new Schema({
 
 userSchema.virtual("friendCount").get(function (){ return this.friends.length});
 
-const User = model("user", userSchema);
+const User = model("User", userSchema);
 
 module.exports = User;
